8571eebafe87c723e3b5fa9e49061dab56994080
Bogdan <35634065+bogdan124@users.noreply.github.com>
2020-08-19 00:51:51 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,61 +1,87 @@
 
 
-
-
-
-
-
 class App extends React.Component {
   
 
   render() {
     return (
       <div class="App-body">
-          <InputComponent nameComponent="Buton1"/>
-          <InputComponent nameComponent="Buton2"/>
-          <InputComponent nameComponent="Buton3"/>
+          <HeaderComponent />
+          <br/>
+          <NameGRID/>
+          
       </div>
     );
   }
 }
 
-
-
-
-
-class ButtonComponent extends React.Component {
-    
-
+class HeaderComponent extends React.Component{
 
   render() {
-    return (
-        <button>{this.props.nameComponent}</button>
+    return(
+      <div class="header-class">
+        <div class="ui large menu">
+          <ButtonComponent name="Home"/>
+          <ButtonComponent name="About" />
+        </div>
+      </div>
     );
   }
 
 }
 
-
-class InputComponent extends  React.Component  {
-  constructor(props) {
-    super(props);
-    // create a ref to store the textInput DOM element
-    this.textInput = React.createRef();
-
-  }
-
-  clickChange(){
-    console.log(this.textInput);
-  }
+class ButtonComponent extends React.Component{
 
   render(){
     return (
-      <div>
-        <input type="text" ref={this.textInput} class="textInput"></input>
-        <ButtonComponent onclick={this.clickChange()} nameComponent={this.props.nameComponent}/>
+      <a class="item">
+          {this.props.name}
+      </a>
+    );
+  }
+
+}
+
+class NameGRID extends App{
+
+  render(){
+    return(
+      <div class="ui grid">
+          <div class="four wide column"></div>
+          <div class="four wide column card"><PhotoComponent/></div>
+          <div class="four wide column card"><DataUserComponent/></div>
+          <div class="four wide column"></div>
+      </div>
+    );
+  }
+
+}
+
+
+class PhotoComponent extends App{
+
+  render(){
+    return(
+      <img class=" medium ui image card" src="picProfile.jpg"></img>
+    );
+  }
+
+}
+class DataUserComponent extends App{
+
+  render(){
+    return(
+      <div class="content">
+        <a class="header nameUse" href="#">Steve Jobes</a>
+        <div class="meta">
+          <a>Last Seen 2 days ago</a>
+        </div>
       </div>
     );
   }
 }
+
+
+
 
 ReactDOM.render(<App/>, document.getElementById('root'))
